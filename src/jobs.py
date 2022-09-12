cee51570134b64533b01dd2143768f84e7c4ad39
Alex Silva <alexcssilva@gmail.com>
2022-09-12 18:38:25 -0300

    feat: added Desafio 01 concluido

--- src/jobs.py
+++ src/jobs.py
@@ -1,18 +1,12 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []
+    with open(path) as csvfile:
+        jobs_reader = csv.DictReader(csvfile, delimiter=",", quotechar='"')
+        jobs_array = []
+        for row in jobs_reader:
+            jobs_array.append(row)
+        return jobs_array
